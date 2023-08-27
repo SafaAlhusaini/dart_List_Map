8837d4c1ed6e985352bf6a501fef6c6b23427336
Safa Sulaiman Alhussaini <111584212+SafaAlhusaini@users.noreply.github.com>
2023-08-27 22:21:46 +0400

    Initial commit

---
+++ bin/dart_application_3.dart
@@ -0,0 +1,32 @@
+void main() {
+  // printMyName("Safa");
+  // printMyName("name1","Safa");
+
+  // int cal1 = cal(y:5, x:50, z:6);
+  // print(cal1);
+
+//   var i = 20; // input
+//   print('fibonacci($i) = ${fibonacci(i)}');
+}
+
+// void printMyName(String name) => print(name);
+
+// NAMES PARAMETERS---------------------------------------
+// void printMyName(String name1, String name2,[String? name3]) {
+//   print(name1);
+//   print(name2);
+//   print("Hello $name3");
+//}
+
+// int cal({int? x, int? y, int? z}) {
+//   return x! +  y! + z!;
+//}
+
+//recrcive function-------------------------------------------
+
+// int fibonacci(int n) {
+// // This is recursive function as it calls itself
+//   return n < 2 ? n : (fibonacci(n - 1) + fibonacci(n - 2));
+// }
+
+//Lambda function or Anonymous function------------------------
